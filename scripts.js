8402dbe6f33d0040995b31a70bd1892efcaa3430
Rafał Chrebela <rafal.chrebela@gmail.com>
2018-02-21 19:42:03 +0100

    Add semicolon scripts.js

--- scripts.js
+++ scripts.js
@@ -1,8 +1,8 @@
 var text = ('Velociraptor is a genus of herbivorous ceratopsid dinosaur that first appeared during the late Maastrichtian stage of the late Cretaceous period.');
 var dinosaur = ('triceratops');
 var dinosaurUpperCased = dinosaur.toUpperCase();
-var textProper = text.replace ('Velociraptor', dinosaurUpperCased);
-var halfOftheSentence = textProper.length/2
+var textProper = text.replace('Velociraptor', dinosaurUpperCased);
+var halfOftheSentence = textProper.length / 2
 var firstHalf = textProper.substr(0, halfOftheSentence);
 
-console.log(firstHalf)
+console.log(firstHalf);
