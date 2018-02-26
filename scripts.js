6978fa76dbf634ce2326fc27ecde5be4a2107589
Rafal-Chrebela <35266261+Rafal-Chrebela@users.noreply.github.com>
2018-02-26 15:29:56 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@ var text = ('Velociraptor is a genus of herbivorous ceratopsid dinosaur that fir
 var dinosaur = ('triceratops');
 var dinosaurUpperCased = dinosaur.toUpperCase();
 var textProper = text.replace('Velociraptor', dinosaurUpperCased);
-var halfOftheSentence = textProper.length / 2
+var halfOftheSentence = textProper.length / 2;
 var firstHalf = textProper.substr(0, halfOftheSentence);
 
 console.log(firstHalf);
